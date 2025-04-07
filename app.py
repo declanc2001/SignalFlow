df72063881390d1c57a14f08d8ffe4ed5a9d9591
declanc2001 <up2129632@myport.ac.uk>
2025-04-07 15:14:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def get_transactions():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not provided
+    app.run(host="0.0.0.0", port=port)
